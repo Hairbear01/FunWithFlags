0f12e5c53cc0334d934b8015df987ea2af555e43
unknown <l018528b@K106-11.student.staffs.ac.uk>
2015-11-03 14:48:19 +0000

    First commit :)

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -7,7 +7,7 @@ USING_NS_CC;
 
 #define COCOS2D_DEBUG 1
 using namespace cocostudio::timeline;
-
+/* This is a test comment */
 Scene* HelloWorld::createScene()
 {
     // 'scene' is an autorelease object
